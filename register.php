f7a72b87c1d96a2dec55d00bb558d3a02781a7e4
Boryana Baeva <borenceto@gmail.com>
2017-03-12 01:57:26 +0200

    added escaping html special characters

--- register.php
+++ register.php
@@ -13,17 +13,17 @@ if (isset($_POST['register']) && isset($_POST['rules']) && isset($_POST['g-recap
         header("Location: register.php");
         exit;
     }
-
+    
     $userService->register(
-        $_POST['firstName'],
-        $_POST['lastName'],
-        $_POST['username'],
-        $_POST['email'],
-        $_POST['password'],
-        $_POST['confirmPassword'],
-        new DateTime($_POST['birthday']),
-        $_POST['gender'],
-        $_POST['phoneNumber']
+        htmlentities($_POST['firstName']),
+        htmlentities($_POST['lastName']),
+        htmlentities($_POST['username']),
+        htmlentities($_POST['email']),
+        htmlentities($_POST['password']),
+        htmlentities($_POST['confirmPassword']),
+        new DateTime(htmlentities($_POST['birthday'])),
+        htmlentities($_POST['gender']),
+        htmlentities($_POST['phoneNumber'])
     );
     header("Location: login.php");
     exit;

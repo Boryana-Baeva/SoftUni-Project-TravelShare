6c47648f218ba6a401bf31a41405901e45e3da5a
Dimitar <asswhoopin@abv.bg>
2017-03-13 22:09:15 +0200

    offer/search ride

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,15 @@
-$('.menuMobile').click(function(){
+$('.menuMobile').click(function () {
     $('.menuItems').slideToggle('slow');
 });
 
-$('.offerRide').click(function(){
-    $('.offerRideExpand:hidden').slideToggle('slow');
+$('.offerRide').click(function () {
+    if ($('.offerRideExpand').is(':hidden')) {
+        $('.offerRideExpand').show('slow');
+    }
 });
+
+
+$('.searchRide').click(function () {
+    $(this).find("h3").toggleClass('down');
+    $('.searchRideExpand').show('slow');
+})

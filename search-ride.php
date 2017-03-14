eaf2de03630e88ac41b4952fd021426badb335d6
Boryana Baeva <borenceto@gmail.com>
2017-03-14 19:00:10 +0200

    display of all posts and searched posts

--- search-ride.php
+++ search-ride.php
@@ -15,35 +15,4 @@ if(isset($_POST['searchRide'])){
     $searchResult = $postsService->findSearched($townFrom, $townTo, $departureDate, $seats);
 
 }
-?>
-<?php $found = false; ?>
-<table border="1" cellspacing="2" cellpadding="5">
-    <thead>
-    <tr>
-        <th>Потребител</th>
-        <th>Публикувано на</th>
-        <th>От</th>
-        <th>До</th>
-        <th>Дата и час на тръгване</th>
-        <th>Цена</th>
-        <th>Свободни места</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($searchResult->getPosts() as $post): ?>
-        <?php $found = true; ?>
-    <tr>
-        <td><?= $post->getAuthor(); ?></td>
-        <td><?= $post->getDatePublished(); ?></td>
-        <td><?= $post->getTownFrom(); ?></td>
-        <td><?= $post->getTownTo(); ?></td>
-        <td><?= $post->getDepartureTime(); ?></td>
-        <td><?= $post->getPrice(); ?></td>
-        <td><?= $post->getSeats(); ?></td>
-    </tr>
-<?php endforeach; ?>
-    <?php if(!$found): ?>
-        <h1>No matches found</h1>
-    <?php endif; ?>
-</tbody>
-</table>
+include_once 'showFoundPosts.html';
--- show-all-posts.php
+++ show-all-posts.php
@@ -10,31 +10,4 @@ $postsService = new PostService($db);
 /** @var $posts \Data\Posts\AllPostsViewData */
 $posts = $postsService->findAll();
 
-?>
-
-<table border="1" cellspacing="2" cellpadding="5">
-    <thead>
-    <tr>
-        <th>Потребител</th>
-        <th>Публикувано на</th>
-        <th>От</th>
-        <th>До</th>
-        <th>Дата и час на тръгване</th>
-        <th>Цена</th>
-        <th>Свободни места</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($posts->getPosts() as $post): ?>
-        <tr>
-            <td><?= $post->getAuthor(); ?></td>
-            <td><?= $post->getDatePublished(); ?></td>
-            <td><?= $post->getTownFrom(); ?></td>
-            <td><?= $post->getTownTo(); ?></td>
-            <td><?= $post->getDepartureTime(); ?></td>
-            <td><?= $post->getPrice(); ?></td>
-            <td><?= $post->getSeats(); ?></td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+include_once 'showAll.html';

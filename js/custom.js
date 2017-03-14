c40c7a6d26ad9ba002f81b9f833fd4174759570e
ruskov3 <ruskovborislav@gmail.com>
2017-03-14 19:52:24 +0200

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -14,3 +14,14 @@ $('.searchRide').click(function () {
     $(this).find("h3").toggleClass('down');
     $('.searchRideExpand').show('slow');
 })
+
+$(document).ready(function () {
+    $(".slide-section").click(function(e){
+        var linkHref = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(linkHref).offset().top
+        }, 1000);
+        e.preventDefault();
+
+    })
+})

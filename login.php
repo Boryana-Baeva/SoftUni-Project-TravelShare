4ce75e22fc8e820163d98fa28736a1d048ddffa4
Boryana Baeva <borenceto@gmail.com>
2017-03-16 22:46:33 +0200

    error handling and displaying error messages

--- login.php
+++ login.php
@@ -2,24 +2,27 @@
 
 require_once 'app.php';
 
+unset($_SESSION['errorMessage']);
 
 if (isset($_POST['login'])) {
+
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     $userService = new \Service\User\UserService($db, $encryptionService);
 
-    $user = $userService->login($email, $password);
+    try{
+        $user = $userService->login($email, $password);
+        $_SESSION['user_id'] = $user->getId();
+        $_SESSION['username'] = $user->getUsername();
+        $_SESSION['email'] = $user->getEmail();
+        header("Location: index.php");
+        exit;
 
-    $_SESSION['user_id'] = $user->getId();
-    $_SESSION['username'] = $user->getUsername();
-    $_SESSION['email'] = $user->getEmail();
-
-
-    header("Location: index.php");
-    exit;
+    } catch (Exception $e){
+        $_SESSION['errorMessage'] = $e->getMessage();
+    }
+    
 }
-
-
 
 include_once 'login.html';
--- register.php
+++ register.php
@@ -3,6 +3,7 @@ use Service\User\UserService;
 
 require_once 'app.php';
 
+unset($_SESSION['errorMessage']);
 
 $userService = new UserService($db, $encryptionService);
 if (isset($_POST['register']) && isset($_POST['rules']) && isset($_POST['g-recaptcha-response'])) {
@@ -13,20 +14,25 @@ if (isset($_POST['register']) && isset($_POST['rules']) && isset($_POST['g-recap
         header("Location: register.php");
         exit;
     }
-    
-    $userService->register(
-        htmlentities($_POST['firstName']),
-        htmlentities($_POST['lastName']),
-        htmlentities($_POST['username']),
-        htmlentities($_POST['email']),
-        htmlentities($_POST['password']),
-        htmlentities($_POST['confirmPassword']),
-        new DateTime(htmlentities($_POST['birthday'])),
-        htmlentities($_POST['gender']),
-        htmlentities($_POST['phoneNumber'])
-    );
-    header("Location: login.php");
-    exit;
+
+    try{
+        $userService->register(
+            htmlentities($_POST['firstName']),
+            htmlentities($_POST['lastName']),
+            htmlentities($_POST['username']),
+            htmlentities($_POST['email']),
+            htmlentities($_POST['password']),
+            htmlentities($_POST['confirmPassword']),
+            new DateTime(htmlentities($_POST['birthday'])),
+            htmlentities($_POST['gender']),
+            htmlentities($_POST['phoneNumber'])
+        );
+        header("Location: login.php");
+        exit;
+    } catch (Exception $e){
+        $_SESSION['errorMessage'] = $e->getMessage();
+    }
+
 }
 
 include_once 'register.html';

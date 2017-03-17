b0a482c50cf1831036ad3fbd3fef03c0c2c53808
Boryana Baeva <borenceto@gmail.com>
2017-03-17 13:19:44 +0200

    exception messages fixed

--- register.php
+++ register.php
@@ -6,7 +6,7 @@ require_once 'app.php';
 unset($_SESSION['errorMessage']);
 
 $userService = new UserService($db, $encryptionService);
-if (isset($_POST['register']) && isset($_POST['rules']) && isset($_POST['g-recaptcha-response'])) {
+if (isset($_POST['register'])) {
 
     $captcha = $_POST['g-recaptcha-response'];
 
@@ -16,6 +16,11 @@ if (isset($_POST['register']) && isset($_POST['rules']) && isset($_POST['g-recap
     }
 
     try{
+
+        if(!isset($_POST['rules'])){
+            throw new Exception("Моля потвърдете, че сте прочели и сте съгласни с правилата");
+        }
+
         $userService->register(
             htmlentities($_POST['firstName']),
             htmlentities($_POST['lastName']),
